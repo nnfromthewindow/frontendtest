84424d3997476cc15411040b5100806a1bb6e9a3
Nicolas Nuccelli <nuccelli@hotmail.com>
2023-12-10 19:35:35 -0300

    deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,41 @@
 import { useNavigate } from "react-router-dom"
+import { useRef } from "react";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import IconButton from "@mui/material/IconButton";
 
 const Home = () =>{
 
     const navigate = useNavigate()
 
+    const menuRef = useRef()
+
     return <>
-  
-    <nav className="navhome">
+  <div className="home">
+  <nav className="navhome">
         <div className="homebar-container">
-        <img src="./public/logo.png" alt="logo" />
-        <div className="menu">
-        <h3 className="item" onClick={()=>navigate('/history')} >01. HISTORY</h3>
-        <h3 className="item" onClick={()=>navigate('/team')}>02. TEAM</h3>
-        </div>
-       
-        </div>
-        
+            <img src="./public/logo.png" alt="logo" />
+            <div className="menu">
+                <h3 className="item" onClick={()=>navigate('/history')} >01. HISTORY</h3>
+                <h3 className="item" onClick={()=>navigate('/team')}>02. TEAM</h3>
+            </div>
+           
+        </div>      
     </nav>
-    <div className="hero">
-        <img src="./public/home.jpg" alt="" />
+    <div className="home-menu">
+            <div className="home-menu__icon">
+                <IconButton aria-label="menu" onClick={()=>{
+                    menuRef.current.style.display='block'}}>
+                    <MenuRoundedIcon sx={{color:'white'}}/>
+                </IconButton> 
+            </div>
+        <div className="home-menu__button" ref={menuRef}>
+                <h3 className="item-menu" onClick={()=>navigate('/history')} >01. HISTORY</h3>
+                <h3 className="item-menu" onClick={()=>navigate('/team')}>02. TEAM</h3>
+            </div>
     </div>
+  
+  </div>
+    
     
     
     
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom"
+import { useRef } from "react";
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import IconButton from "@mui/material/IconButton";
 
 const Navbar = () => {
   
     const navigate = useNavigate()
   
+    const menuRef = useRef()
+
     return (
     <nav className="navbar">
         <div className="navbar-box">
@@ -15,8 +20,27 @@ const Navbar = () => {
                 <h3 className="item" onClick={()=>navigate('/history')} >01. HISTORY</h3>
                 <h3 className="item" onClick={()=>navigate('/team')}>02. TEAM</h3>
             </div>
+           
         </div>
-    </nav>
+        <div className="menu-container">
+        <div className="menu-icon" >
+                <IconButton  aria-label="menu" onClick={()=>{
+                    menuRef.current.style.display='block'}}>
+                    <MenuRoundedIcon />
+                </IconButton> 
+            </div>   
+        <div className="menu-button" ref={menuRef}>
+      
+            <h3 className="item-menu" onClick={()=>{navigate('/history')
+            menuRef.current.style.display='none'
+        }} >01. HISTORY</h3>
+            <h3 className="item-menu" onClick={()=>{navigate('/team')
+        menuRef.current.style.display='none'}}>02. TEAM</h3>
+        </div>
+        
+        </div>
+        
+</nav>
   )
 }
 
